b58a22c7328212fef88ec2010c604f09e4729fc1
tarang <tarang.koradiya@webcluesglobal.com>
2022-06-30 12:17:11 +0530

    nextjs todo list

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import { FontAwesomeIcon } from '../node_modules/@fortawesome/react-fontawesome/index'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/components/Todoform.tsx
@@ -0,0 +1,82 @@
+import React, {useContext, useEffect, useState} from "react";
+import { FontAwesomeIcon } from "../../node_modules/@fortawesome/react-fontawesome/index";
+
+const Todoform = ()=>{
+
+   // const title = useContext(TodoTitle);
+   // const Notifi = useContext(NotificationTitle);
+
+    const [Workn, setWorkn] = useState('');
+
+    const[Tododata, setTododata] = useState([]);
+   
+    const changeval = (event)=>{
+        setWorkn(event.target.value);
+    }
+   
+
+    const formSubmit = (event)=>{
+        event.preventDefault();
+        console.log(event.target['workname'].value)
+
+        setTododata((oldTododata)=>{
+            return [...oldTododata, event.target['workname'].value];
+           
+        })
+        
+        setWorkn('');   
+      //  useNotifiCation(Notifi,Tododata);         
+    }
+
+    const deletetodo = (key)=>{
+        setTododata((oldTododata)=>{
+            return oldTododata.filter((arrElem,index)=>{
+                return index !== key;
+            }
+    )})     
+   // useNotifiCation(Notifi,Tododata);  
+    }
+
+
+    return(
+        <>
+        <section>
+            <header >
+                <h1>WORKS TO BE DONE</h1>
+                
+                <form onSubmit={formSubmit}>
+                <input 
+                onChange={changeval}
+                className="inputname"
+                type="text"
+                name="workname"
+                value={Workn}
+                placeholder="Enter the Work"
+                />
+                <button id="input-form-btn"><i className="fa-solid fa fa-plus"></i></button>
+                </form>
+            </header>
+
+            <div className="container">
+                {Tododata.map((todo,index)=>(
+                    
+                    <>
+                <div className="task">
+                <div className="content">
+                   {todo}
+                </div>
+                <div className="btn">
+                <button id="del" onClick={()=>deletetodo(index)}><i className="fa-solid fa-trash-can fa fa-trash-o"></i></button>
+                </div>
+                </div>
+                    </>
+                ))}
+                
+
+            </div>
+
+        </section>
+        </>
+    )
+}
+export default Todoform;
