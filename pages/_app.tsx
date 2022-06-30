2c8f4fa09d160700490ae151c1003ca181991b9d
tarang <tarang.koradiya@webcluesglobal.com>
2022-06-30 12:46:32 +0530

    nextjs todo list new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import { FontAwesomeIcon } from '../node_modules/@fortawesome/react-fontawesome/index'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/components/Todoform.tsx
+++ pages/components/Todoform.tsx
@@ -1,5 +1,4 @@
 import React, {useContext, useEffect, useState} from "react";
-import { FontAwesomeIcon } from "../../node_modules/@fortawesome/react-fontawesome/index";
 
 const Todoform = ()=>{
 
